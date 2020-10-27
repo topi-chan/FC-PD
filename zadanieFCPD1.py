7f86f221cb34b669597a0b99098a3a10db763a56
Maciej Topolewski <maciej@top-project.com.pl>
2020-10-27 01:08:38 +0100

    umieszczam pierwszą wersję rozwiązanego zadania

--- zadanieFCPD1.py
+++ zadanieFCPD1.py
@@ -49,5 +49,77 @@ x22 = 1.165733399
 x23 = -0.404186718
 x24 = 1.499708521
 
-def wydruk(x):
-    print(x)
+#wydruk
+print("Podaję wysokość kredytu do spłaty w złotych polskich:")
+
+styczen = (1 + ((x1+3)/1200)) * kredyt_flt - rata_flt
+print("Styczeń: ", styczen)
+
+luty = (1 + ((x2+3)/1200)) * styczen - rata_flt
+print("Luty: ", luty)
+
+marzec = (1 + ((x3+3)/1200)) * luty - rata_flt
+print("Marzec: ", marzec)
+
+kwiecien = (1 + ((x4+3)/1200)) * marzec - rata_flt
+print("Kwiecień: ", kwiecien)
+
+maj = (1 + ((x5+3)/1200)) * kwiecien - rata_flt
+print("Maj: ", maj)
+
+czerwiec = (1 + ((x6+3)/1200)) * maj - rata_flt
+print("Czerwiec: ", czerwiec)
+
+lipiec = (1 + ((x7+3)/1200)) * czerwiec - rata_flt
+print("Lipiec: ", lipiec)
+
+sierpien = (1 + ((x8+3)/1200)) * lipiec - rata_flt
+print("Sierpień: ", sierpien)
+
+wrzesien = (1 + ((x9+3)/1200)) * sierpien - rata_flt
+print("Wrzesień: ", wrzesien)
+
+pazdziernik = (1 + ((x10+3)/1200)) * wrzesien - rata_flt
+print("Październik: ", pazdziernik)
+
+listopad = (1 + ((x11+3)/1200)) * pazdziernik - rata_flt
+print("Listopad: ", listopad)
+
+grudzien = (1 + ((x12+3)/1200)) * listopad - rata_flt
+print("Grudzień: ", grudzien)
+
+styczen1 = (1 + ((x13+3)/1200)) * grudzien - rata_flt
+print("Styczeń: ", styczen1)
+
+luty1 = (1 + ((x14+3)/1200)) * styczen1 - rata_flt
+print("Luty: ", luty1)
+
+marzec1 = (1 + ((x15+3)/1200)) * luty1 - rata_flt
+print("Marzec: ", marzec1)
+
+kwiecien1 = (1 + ((x16+3)/1200)) * marzec1 - rata_flt
+print("Kwiecień: ", kwiecien1)
+
+maj1 = (1 + ((x17+3)/1200)) * kwiecien1 - rata_flt
+print("Maj: ", maj1)
+
+czerwiec1 = (1 + ((x18+3)/1200)) * maj1 - rata_flt
+print("Czerwiec: ", czerwiec1)
+
+lipiec1 = (1 + ((x19+3)/1200)) * czerwiec1 - rata_flt
+print("Lipiec: ", lipiec1)
+
+sierpien1 = (1 + ((x20+3)/1200)) * lipiec1 - rata_flt
+print("Sierpień: ", sierpien1)
+
+wrzesien1 = (1 + ((x21+3)/1200)) * sierpien1 - rata_flt
+print("Wrzesień: ", wrzesien1)
+
+pazdziernik1 = (1 + ((x22+3)/1200)) * wrzesien1 - rata_flt
+print("Październik: ", pazdziernik1)
+
+listopad1 = (1 + ((x23+3)/1200)) * pazdziernik1 - rata_flt
+print("Listopad: ", listopad1)
+
+grudzien1 = (1 + ((x24+3)/1200)) * listopad1 - rata_flt
+print("Grudzień: ", grudzien1)
