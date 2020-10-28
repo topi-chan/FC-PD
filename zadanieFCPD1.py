de3162d26b06f3cbbf7f9b97d4112afb31998eaa
Maciek <maciek@host9.private.net>
2020-10-28 09:40:39 +0100

    poprawiłem oprocentowanie i uściśliłem komentarz

--- zadanieFCPD1.py
+++ zadanieFCPD1.py
@@ -23,7 +23,7 @@ except:
     print("Wpisz wartość liczbową")
     quit()
 
-# poszczególne miesiące po kolei
+# poszczególne miesiące po kolei - inflacja
 x1 = 1.59282448436825
 x2 = -0.453509101
 x3 = 2.324671717
@@ -52,74 +52,74 @@ x24 = 1.499708521
 #wydruk
 print("Podaję wysokość kredytu do spłaty w złotych polskich:")
 
-styczen = (1 + ((x1+3)/1200)) * kredyt_flt - rata_flt
+styczen = (1 + ((x1+oprocentowanie_flt)/1200)) * kredyt_flt - rata_flt
 print("Styczeń: ", styczen)
 
-luty = (1 + ((x2+3)/1200)) * styczen - rata_flt
+luty = (1 + ((x2+oprocentowanie_flt)/1200)) * styczen - rata_flt
 print("Luty: ", luty)
 
-marzec = (1 + ((x3+3)/1200)) * luty - rata_flt
+marzec = (1 + ((x3+oprocentowanie_flt)/1200)) * luty - rata_flt
 print("Marzec: ", marzec)
 
-kwiecien = (1 + ((x4+3)/1200)) * marzec - rata_flt
+kwiecien = (1 + ((x4+oprocentowanie_flt)/1200)) * marzec - rata_flt
 print("Kwiecień: ", kwiecien)
 
-maj = (1 + ((x5+3)/1200)) * kwiecien - rata_flt
+maj = (1 + ((x5+oprocentowanie_flt)/1200)) * kwiecien - rata_flt
 print("Maj: ", maj)
 
-czerwiec = (1 + ((x6+3)/1200)) * maj - rata_flt
+czerwiec = (1 + ((x6+oprocentowanie_flt)/1200)) * maj - rata_flt
 print("Czerwiec: ", czerwiec)
 
-lipiec = (1 + ((x7+3)/1200)) * czerwiec - rata_flt
+lipiec = (1 + ((x7+oprocentowanie_flt)/1200)) * czerwiec - rata_flt
 print("Lipiec: ", lipiec)
 
-sierpien = (1 + ((x8+3)/1200)) * lipiec - rata_flt
+sierpien = (1 + ((x8+oprocentowanie_flt)/1200)) * lipiec - rata_flt
 print("Sierpień: ", sierpien)
 
-wrzesien = (1 + ((x9+3)/1200)) * sierpien - rata_flt
+wrzesien = (1 + ((x9+oprocentowanie_flt)/1200)) * sierpien - rata_flt
 print("Wrzesień: ", wrzesien)
 
-pazdziernik = (1 + ((x10+3)/1200)) * wrzesien - rata_flt
+pazdziernik = (1 + ((x10+oprocentowanie_flt)/1200)) * wrzesien - rata_flt
 print("Październik: ", pazdziernik)
 
-listopad = (1 + ((x11+3)/1200)) * pazdziernik - rata_flt
+listopad = (1 + ((x11+oprocentowanie_flt)/1200)) * pazdziernik - rata_flt
 print("Listopad: ", listopad)
 
-grudzien = (1 + ((x12+3)/1200)) * listopad - rata_flt
+grudzien = (1 + ((x12+oprocentowanie_flt)/1200)) * listopad - rata_flt
 print("Grudzień: ", grudzien)
 
-styczen1 = (1 + ((x13+3)/1200)) * grudzien - rata_flt
+styczen1 = (1 + ((x13+oprocentowanie_flt)/1200)) * grudzien - rata_flt
 print("Styczeń: ", styczen1)
 
-luty1 = (1 + ((x14+3)/1200)) * styczen1 - rata_flt
+luty1 = (1 + ((x14+oprocentowanie_flt)/1200)) * styczen1 - rata_flt
 print("Luty: ", luty1)
 
-marzec1 = (1 + ((x15+3)/1200)) * luty1 - rata_flt
+marzec1 = (1 + ((x15+oprocentowanie_flt)/1200)) * luty1 - rata_flt
 print("Marzec: ", marzec1)
 
-kwiecien1 = (1 + ((x16+3)/1200)) * marzec1 - rata_flt
+kwiecien1 = (1 + ((x16+oprocentowanie_flt)/1200)) * marzec1 - rata_flt
 print("Kwiecień: ", kwiecien1)
 
-maj1 = (1 + ((x17+3)/1200)) * kwiecien1 - rata_flt
+maj1 = (1 + ((x17+oprocentowanie_flt)/1200)) * kwiecien1 - rata_flt
 print("Maj: ", maj1)
 
-czerwiec1 = (1 + ((x18+3)/1200)) * maj1 - rata_flt
+czerwiec1 = (1 + ((x18+oprocentowanie_flt)/1200)) * maj1 - rata_flt
 print("Czerwiec: ", czerwiec1)
 
-lipiec1 = (1 + ((x19+3)/1200)) * czerwiec1 - rata_flt
+lipiec1 = (1 + ((x19+oprocentowanie_flt)/1200)) * czerwiec1 - rata_flt
 print("Lipiec: ", lipiec1)
 
-sierpien1 = (1 + ((x20+3)/1200)) * lipiec1 - rata_flt
+sierpien1 = (1 + ((x20+oprocentowanie_flt)/1200)) * lipiec1 - rata_flt
 print("Sierpień: ", sierpien1)
 
-wrzesien1 = (1 + ((x21+3)/1200)) * sierpien1 - rata_flt
+wrzesien1 = (1 + ((x21+oprocentowanie_flt)/1200)) * sierpien1 - rata_flt
 print("Wrzesień: ", wrzesien1)
 
-pazdziernik1 = (1 + ((x22+3)/1200)) * wrzesien1 - rata_flt
+pazdziernik1 = (1 + ((x22+oprocentowanie_flt)/1200)) * wrzesien1 - rata_flt
 print("Październik: ", pazdziernik1)
 
-listopad1 = (1 + ((x23+3)/1200)) * pazdziernik1 - rata_flt
+listopad1 = (1 + ((x23+oprocentowanie_flt)/1200)) * pazdziernik1 - rata_flt
 print("Listopad: ", listopad1)
 
-grudzien1 = (1 + ((x24+3)/1200)) * listopad1 - rata_flt
+grudzien1 = (1 + ((x24+oprocentowanie_flt)/1200)) * listopad1 - rata_flt
 print("Grudzień: ", grudzien1)
