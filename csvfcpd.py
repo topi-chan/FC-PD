a44f0df11431c5f86c4da6c6d7c35eda76a6c273
Maciej Topolewski <maciej@top-project.com.pl>
2020-11-28 15:01:18 +0100

    dalej robię, może być problem z argv[3:] w fukcji

--- csvfcpd.py
+++ csvfcpd.py
@@ -10,6 +10,8 @@ with open('/Users/maciek/addresses.csv', 'r') as f:
     for line in reader:
         print(line)
     lines = list(reader)
+print(lines)
+
 
 def csv_write (file, line, column, content):
     column_counter = 0
@@ -17,20 +19,22 @@ def csv_write (file, line, column, content):
         with open(file, "w") as f:
             writer = csv.writer(f)
             while True:
-
+                x =
+                y = next(reader)
             csv_file = writer.writerow(["Maciej", "Topolewski", "Katowice", "PL"])
     except:
         print(os.listdir(file))
     return csv_file
 
-def csv_save(directory, file_to_save):
-    save = os.path.join(directory, 'addresses.csv')
-    file = open(file_to_save, 'w')
-    file.write(csv_file)
+def csv_save(directory, file_name, file_to_save):
+    save = os.path.join(directory, file_name)
+    file = open(save, 'w')
+    file.write(file_to_save)
     file.close()
 
+csv_write(sys.argv[1], sys.argv[3], sys.argv[4], sys.argv[5])
 
-csv_save(sys.argv[2], csv_file)
+csv_save(sys.argv[2], 'addresses.csv', csv_file)
 
 # with open('/Users/maciek/csv/addresses.csv', "w") as f:
 #     writer = csv.writer(f)
@@ -40,7 +44,6 @@ csv_save(sys.argv[2], csv_file)
 df = pd.read_csv('/Users/maciek/addresses.csv')
 print(df)
 
-print(lines)
 
 
 
