f1334d28c9bf7e5cb2f83bf24787119c4821d599
Maciej Topolewski <maciej@top-project.com.pl>
2020-11-28 13:26:30 +0100

    próba rozwiązania CSV

--- csvfcpd.py
+++ csvfcpd.py
@@ -13,14 +13,6 @@ with open('/Users/maciek/csv/addresses.csv', "w", newline="") as f:
 
 f.close()
 
-num_rows = 0
-with open('/Users/maciek/addresses.csv', newline="") as f:
-    reader = csv.reader(f)
-    for line in reader:
-        print(" ".join(line))
-        num_rows += 1
-print(num_rows)
-
 df = pd.read_csv('/Users/maciek/addresses.csv')
 print(df)
 
@@ -46,3 +38,12 @@ print(lines)
 # except:
 #     print(os.listdir(loc))
 # '''
+
+
+# num_rows = 0
+# with open('/Users/maciek/addresses.csv', newline="") as f:
+#     reader = csv.reader(f)
+#     for line in reader:
+#         print(" ".join(line))
+#         num_rows += 1
+# print(num_rows)
