a476e3b8b10cb969dca919ad83a026a4b6e4c9a7
Maciej Topolewski <maciej@top-project.com.pl>
2020-11-28 13:49:00 +0100

    próbuję zrobić przez funkcję

--- csvfcpd.py
+++ csvfcpd.py
@@ -3,13 +3,26 @@ import sys
 import pandas as pd
 import os
 
-with open('/Users/maciek/csv/addresses.csv', newline="") as f:
+with open('/Users/maciek/addresses.csv', 'r') as f:
     reader = csv.reader(f)
+    next(reader)
+    next(reader)
+    for line in reader:
+        print(line)
     lines = list(reader)
 
-with open('/Users/maciek/csv/addresses.csv', "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Maciej", "Topolewski", "Katowice", "PL"])
+def csv_write (file, directory, line, column, content):
+    try:
+        with open(file, "w") as f:
+            writer = csv.writer(f)
+
+    except:
+        print(os.listdir(file))
+
+
+# with open('/Users/maciek/csv/addresses.csv', "w") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["Maciej", "Topolewski", "Katowice", "PL"])
 
 f.close()
 
