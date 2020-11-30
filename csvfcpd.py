9e69d2f332abf3689d4fbfad91659cc69a314e4b
Maciej Topolewski <maciej@top-project.com.pl>
2020-11-30 17:59:01 +0100

    zadanie 5

--- csvfcpd.py
+++ csvfcpd.py
@@ -1,10 +1,8 @@
 import csv
 import sys
-import pandas as pd
 import os
 
-
-def csv_read (file):
+def csv_read(file):
     try:
         list = []
         with open(file, "r") as f:
@@ -13,110 +11,31 @@ def csv_read (file):
                 list.append(line)
     except:
         print(os.listdir(file))
+        quit()
     return list
 
-def csv_define (list):
+def csv_define(original_list):
     for arg in sys.argv[3:]:
-        change = change.split(",")
-        Y = int(change[0])
-        X = int(change[1])
-        value = change[2]
-    # count_column = 0
-    # for count_row, line in enumerate(reader):
-    #     count_column = len(line)
-    # items = count_row + count_column
-    # list[row]
-    list[row][column] = content
+        arg = arg.split(",")
+        X = int(arg[0])
+        Y = int(arg[1])
+        content = arg[2]
+        original_list[X][Y] = content
+    new_list = original_list
+    return new_list
 
-#     for change in argv[3:]:
-
-#         csvList[Y][X] = value
-
-
-
-#file_to_save = csv_file
-def csv_save(directory, file_name, file_to_save):
-    writer
-    for l in list:
-        writer.writerow(l)
+def csv_save(original_file, directory, file_name, file_to_save):
+    with open(original_file, "w") as f:
+        writer = csv.writer(f)
+    for line in file_to_save:
+        csv_writer.writerow(line)
     save = os.path.join(directory, file_name)
-    file = open(save, 'w')
-    file.write(file_to_save)
     file.close()
 
 list = csv_read(sys.argv[1])
 
-print(list[3][2])
+new_list = csv_define(list)
+
+csv_save(sys.argv[1], sys.argv[2], 'addresses.csv', new_list)
 
 quit()
-
-csv_define()
-
-csv_save(sys.argv[2], 'addresses.csv', list)
-
-
-
-# df = pd.read_csv('/Users/maciek/addresses.csv')
-# print(df)
-
-
-f_open = sys.argv[1]
-f_read = sys.argv[2]
-for arg in sys.argv[3:]:
-     x
-
-
-
-
-
-# with open('/Users/maciek/addresses.csv', 'r') as f:
-#     reader = csv.reader(f)
-#     # next(reader)
-#     for line in reader:
-#         list.append(line)
-# # for l in list:
-# #     print(l)
-#
-# quit()
-
-
-
-# with open('/Users/maciek/csv/addresses.csv', "w") as f:
-#     writer = csv.writer(f)
-#     writer.writerow(["Maciej", "Topolewski", "Katowice", "PL"])
-
-
-
-#
-# f = open('mylist.csv', 'r')
-# reader = csv.reader(f)
-# mylist = list(reader)
-# f.close()
-# mylist[1][3] = 'X'
-# my_new_list = open('mylist.csv', 'w', newline = '')
-# csv_writer = csv.writer(my_new_list)
-# csv_writer.writerows(mylist)
-# my_new_list.close()
-
-
-#import petl - pandas pod csv
-
-
-#
-# '''
-# loc = sys.argv[1]
-# try:
-#     df = pd.read_csv(loc)
-#     print(df)
-# except:
-#     print(os.listdir(loc))
-# '''
-
-
-# num_rows = 0
-# with open('/Users/maciek/addresses.csv', newline="") as f:
-#     reader = csv.reader(f)
-#     for line in reader:
-#         print(" ".join(line))
-#         num_rows += 1
-# print(num_rows)
