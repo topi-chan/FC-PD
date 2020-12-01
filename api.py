a9b2dced2d5e8bbe3fc10d38437d4eb5ac60e6be
Maciek <maciek@host9.private.net>
2020-12-01 12:20:43 +0100

    doszedłem jak wrzucic api z argv

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
 import requests
+import sys
 
 url = "https://api.climacell.co/v3/weather/forecast/daily"
 
-querystring = {"lat":"50","lon":"19","unit_system":"si","start_time":"now",
-"end_time":"2020-12-01T15:00:00","fields":"precipitation_probability",
-"apikey":"PFtJhTkCKd9Xaua1zq70f8eeCjm6Bq0c"}
+def api_weather(api_key, forecast_date):
+    querystring = {"lat":"50","lon":"19","unit_system":"si",
+    "start_time":forecast_date,
+    "end_time":"2020-12-01T15:00:00",
+    "fields":"precipitation_probability",
+    "apikey":api_key}
+    return querystring
+
+querystring = api_weather("{}".format(sys.argv[1]), "now")
+
 
 response = requests.request("GET", url, params=querystring)
 
+
 print(response.text)
+
+#PFtJhTkCKd9Xaua1zq70f8eeCjm6Bq0c
+#"now"
