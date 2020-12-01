177b12158be52bf74981e0aabf29d00053093f2f
Maciek <maciek@host9.private.net>
2020-12-01 12:43:47 +0100

    mam pobieranie daty z argv

--- api.py
+++ api.py
@@ -1,23 +1,25 @@
 import requests
 import sys
+import json
 
 url = "https://api.climacell.co/v3/weather/forecast/daily"
 
 def api_weather(api_key, forecast_date):
     querystring = {"lat":"50","lon":"19","unit_system":"si",
     "start_time":forecast_date,
-    "end_time":"2020-12-01T15:00:00",
+    "end_time":forecast_date,
     "fields":"precipitation_probability",
     "apikey":api_key}
     return querystring
 
-querystring = api_weather("{}".format(sys.argv[1]), "now")
+querystring = api_weather("{}".format(sys.argv[1]), "{}".format(sys.argv[2]))
 
 
 response = requests.request("GET", url, params=querystring)
 
 
-print(response.text)
+print(response.json())
+
 
 #PFtJhTkCKd9Xaua1zq70f8eeCjm6Bq0c
 #"now"
