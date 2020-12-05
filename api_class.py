562f3a177e35a00b23327037cc336711ee7cd1bb
Maciek <maciek@host9.private.net>
2020-12-05 15:16:39 +0100

    tutaj mam bardziej przeklejki na razie i pomysly - zad 7

--- api_class.py
+++ api_class.py
@@ -33,6 +33,56 @@ class WeatherForecast(self, api_key):
         tups = d.items()
         print(tups)
 
+    def csv_read(file):
+        list = []
+        with open(file, "r") as f:
+            reader = csv.reader(f)
+            for line in reader:
+                list.append(line)
+
+    def csv_define(original_list):
+        for arg in sys.argv[3:]:
+            arg = arg.split(",")
+            X = int(arg[0])
+            Y = int(arg[1])
+            content = arg[2]
+            original_list[X][Y] = content
+        new_list = original_list
+        return new_list
+
+    def csv_save(original_file, directory, file_name, file_to_save):
+        with open(original_file, "w") as f:
+            writer = csv.writer(f)
+        for line in file_to_save:
+            writer.writerow(line)
+        save = os.path.join(directory, file_name)
+        f.close()
+
+
+
+class FileReader:
+    def __init__(self, filepath):
+        self.fp = open(filepath)
+        self.lines = []
+        self.done = False
+
+    def __iter__(self):
+        if self.done:
+            return iter(self.lines)
+        return self
+
+    def __next__(self):
+        line = self.fp.readline()
+        if not line:
+            self.done = True
+            self.fp.close()
+            raise StopIteration
+        self.lines.append(line[:-1])
+        return line[:-1]
+
+for line in FileReader("dane.txt"):
+    print(line)
+
 
 url = "https://api.climacell.co/v3/weather/forecast/daily"
 
