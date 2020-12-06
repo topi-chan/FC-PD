d1cf1a463a044bad9af2baca2838a4069af66934
Maciej Topolewski <maciej@top-project.com.pl>
2020-12-06 14:15:07 +0100

    nie działa odwoływanie się do funkcji wewnątrz klasy

--- csvfjs.py
+++ csvfjs.py
@@ -1,34 +1,52 @@
 import csv, os, sys, json, pickle
 import pandas as pd
 
-class FileReader(object):
+
+class FileReader:
     """Allows you to open and rewrite csv/json/pickle files"""
 
     def __init__(self):
         self.list = []
+        self.new_list = []
 
-    def file_read(file):
-        list = []
+    def file_read(self, file):
         try:
             with open(file, "r") as f:
                 reader = csv.reader(f)
+                for line in reader:
+                    self.list.append(line)
+                self.csv_define(self.list, sys.argv[3:])
         except:
             pass
         try:
             with open(file) as json_file:
                 reader = json.load(json_file)
+                for line in reader:
+                    self.list.append(line)
+                self.json_define()
         except:
             pass
         try:
             reader = pd.read_pickle(file)
+            for line in reader:
+                self.list.append(line)
+            self.pickle_define()
         except:
-            print(os.listdir(file))
-            quit()
-        for line in reader:
-            list.append(line)
-        return list
+            # print(os.listdir(file))
+            # quit()
+            pass
 
-    def csv_define(original_list, arg_number):
+    def csv_define(self, original_list, arg_number):
+        for arg in arg_number:
+            arg = arg.split(",")
+            X = int(arg_number[0])
+            Y = int(arg_number[1])
+            content = arg_number[2]
+            original_list[X][Y] = content
+        self.new_list = original_list
+        print(self.new_list)
+
+    def pickle_define(self, original_list, arg_number):
         for arg in arg_number:
             arg = arg.split(",")
             X = int(arg_number[0])
@@ -39,18 +57,7 @@ class FileReader(object):
         print(new_list)
         return new_list
 
-    def pickle_define(original_list, arg_number):
-        for arg in arg_number:
-            arg = arg.split(",")
-            X = int(arg_number[0])
-            Y = int(arg_number[1])
-            content = arg_number[2]
-            original_list[X][Y] = content
-        new_list = original_list
-        print(new_list)
-        return new_list
-
-    def csv_save(original_file, directory, file_name, file_to_save):
+    def csv_save(self, original_file, directory, file_name, file_to_save):
         with open(os.path.join(directory, file_name), "w") as f:
             writer = csv.writer(f)
             for line in file_to_save:
@@ -61,9 +68,10 @@ class FileReader(object):
 #
 # def pickle_save
 fr = FileReader()
-list = fr.file_read(sys.argv[1])
-print(list)
+fr.file_read(sys.argv[1])
 quit()
+
+print(fr.list)
 
 new_list = csv_define(list, sys.argv[3:])
 print(new_list)
