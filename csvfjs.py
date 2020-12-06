cd511245d1b45f066f82f0e615b58b995d05a76d
Maciej Topolewski <maciej@top-project.com.pl>
2020-12-06 13:34:59 +0100

    wersja z klasą

--- csvfjs.py
+++ csvfjs.py
@@ -1,50 +1,72 @@
 import csv, os, sys, json, pickle
 import pandas as pd
 
-def file_read(file):
-    list = []
-    try:
-        with open(file, "r") as f:
-            reader = csv.reader(f)
-    except:
-        pass
-    try:
-        with open(file) as json_file:
-            reader = json.load(json_file)
-    except:
-        pass
-    try:
-        reader = pd.read_pickle(file)
-    except:
-        print(os.listdir(file))
-        quit()
-    for line in reader:
-        list.append(line)
-    return list
+class FileReader(object):
+    """Allows you to open and rewrite csv/json/pickle files"""
 
-def csv_define(original_list, arg_number):
-    for arg in arg_number:
-        arg = arg.split(",")
-        X = int(arg_number[0])
-        Y = int(arg_number[1])
-        content = arg_number[2]
-        original_list[X][Y] = content
-    new_list = original_list
-    print(new_list)
-    return new_list
+    def __init__(self):
+        self.list = []
 
-def csv_save(original_file, directory, file_name, file_to_save):
-    with open(os.path.join(directory, file_name), "w") as f:
-        writer = csv.writer(f)
-        for line in file_to_save:
-            writer.writerow(line)
-        f.close()
+    def file_read(file):
+        list = []
+        try:
+            with open(file, "r") as f:
+                reader = csv.reader(f)
+        except:
+            pass
+        try:
+            with open(file) as json_file:
+                reader = json.load(json_file)
+        except:
+            pass
+        try:
+            reader = pd.read_pickle(file)
+        except:
+            print(os.listdir(file))
+            quit()
+        for line in reader:
+            list.append(line)
+        return list
 
+    def csv_define(original_list, arg_number):
+        for arg in arg_number:
+            arg = arg.split(",")
+            X = int(arg_number[0])
+            Y = int(arg_number[1])
+            content = arg_number[2]
+            original_list[X][Y] = content
+        new_list = original_list
+        print(new_list)
+        return new_list
 
-list = file_read(sys.argv[1])
+    def pickle_define(original_list, arg_number):
+        for arg in arg_number:
+            arg = arg.split(",")
+            X = int(arg_number[0])
+            Y = int(arg_number[1])
+            content = arg_number[2]
+            original_list[X][Y] = content
+        new_list = original_list
+        print(new_list)
+        return new_list
+
+    def csv_save(original_file, directory, file_name, file_to_save):
+        with open(os.path.join(directory, file_name), "w") as f:
+            writer = csv.writer(f)
+            for line in file_to_save:
+                writer.writerow(line)
+            f.close()
+
+# def json_save
+#
+# def pickle_save
+fr = FileReader()
+list = fr.file_read(sys.argv[1])
 print(list)
 quit()
+
 new_list = csv_define(list, sys.argv[3:])
+print(new_list)
 
 csv_save(sys.argv[1], sys.argv[2], sys.argv[1], new_list)
 
