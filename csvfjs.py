19eb600f5c888a49dfc5134a04eb9d1e288b51bd
Maciej Topolewski <maciej@top-project.com.pl>
2020-12-06 14:50:18 +0100

    jestem na zapisywaniu csv w klasie

--- csvfjs.py
+++ csvfjs.py
@@ -47,7 +47,7 @@ class FileReader:
             content = arg_number[2]
             original_list[X][Y] = content
         self.new_list = original_list
-        print(self.new_list)
+        self.csv_save(self.arg2, self.arg1, self.new_list)
 
     def pickle_define(self, original_list, arg_number):
         for arg in arg_number:
@@ -60,7 +60,7 @@ class FileReader:
         print(new_list)
         return new_list
 
-    def csv_save(self, original_file, directory, file_name, file_to_save):
+    def csv_save(self, directory, file_name, file_to_save):
         with open(os.path.join(directory, file_name), "w") as f:
             writer = csv.writer(f)
             for line in file_to_save:
